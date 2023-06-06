9340904fdd7e2198e935557b52a9d61ae8dcc10a
BardinPetr <bardin.petr@gmail.com>
2023-06-06 20:53:50 +0300

    resource bundle generator

--- client/clientGUI/src/main/java/ru/bardinpetr/itmo/lab5/clientgui/i18n/data/GUITexts.java
+++ client/clientGUI/src/main/java/ru/bardinpetr/itmo/lab5/clientgui/i18n/data/GUITexts.java
@@ -6,6 +6,14 @@ public class GUITexts extends ListResourceBundle {
     @Override
     protected Object[][] getContents() {
         return new Object[][]{
+                {"loginform.label5.text", "text"},
+                {"loginpage.checkBox1.text", "text"},
+                {"loginpage.button2.text", "text"},
+                {"loginpage.button3.text", "text"},
+                {"loginpage.this.title", "Login"},
+                {"loginpage.label1.text", "text"},
+                {"loginpage.label2.text", "text"},
+                {"loginpage.label3.text", "text"},
                 {"login", "Login"},
                 {"login_btn", "Login"},
                 {"register_btn", "Register"}
---
+++ client/clientGUI/utils/rbConvert.py
@@ -0,0 +1,59 @@
+import os
+import sys
+from translate import Translator
+
+BASE_LANG = "en"
+TARGET_LANGS = ["ru"]
+TRANSLATORS = {i: Translator(from_lang=BASE_LANG, to_lang=i) for i in TARGET_LANGS}
+
+class_template = lambda package, base_name, contents: \
+    f"""package {package};
+
+import java.util.ListResourceBundle;
+
+public class {base_name} extends ListResourceBundle {{
+    @Override
+    protected Object[][] getContents() {{
+        return new Object[][] {{
+{contents}
+        }};
+    }}
+}}"""
+
+
+def translate(from_lang, to_lang, contents):
+    if to_lang is None:
+        return contents
+    for k, v in contents.items():
+        print(k, v, TRANSLATORS[to_lang].translate(v))
+    return {k: TRANSLATORS[to_lang].translate(v) for k, v in contents.items()}
+
+
+def main():
+    package, target_dir = sys.argv[1:]
+
+    for root, _, files in os.walk(target_dir):
+        for f_path in files:
+            full_path = f"{root}/{f_path}"
+            if not f_path.endswith('.properties'): continue
+
+            with open(full_path, 'r') as f:
+                contents = f.read()
+
+            contents = {(pair := line.split('='))[0]: pair[1] for i in contents.split('\n') if (line := i.strip())}
+
+            base_name = f_path.split('.')[0]
+
+            for target_lang in [None, *TARGET_LANGS]:
+                translated_name = base_name + ('_' + target_lang if target_lang else '')
+                target_file = f"{root}/{translated_name}.java"
+                with open(target_file, 'w') as target_file:
+                    contents_str = translate(BASE_LANG, target_lang, contents)
+                    contents_str = [f'{12 * " "}{{"{k}", "{v}"}}' for k, v in contents_str.items()]
+                    contents_str = ',\n'.join(contents_str)
+                    text = class_template(package, translated_name, contents_str)
+                    target_file.write(text)
+
+
+if __name__ == '__main__':
+    main()
